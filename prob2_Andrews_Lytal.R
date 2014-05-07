e8ba4e967a94b1febe590e4b532add8a44623611
njlytal <njlytal@ucdavis.edu>
2014-05-07 15:01:08 -0700

    Progress on Prob 2
    Made a function, still needs testing

--- prob2_Andrews_Lytal.R
+++ prob2_Andrews_Lytal.R
@@ -16,6 +16,37 @@
 # Either sample conditionally in the given int. range 
 # OR change all samples out of the given int. range to be 0
 
+
+# So we sample from g(x) and place the resulting samples in
+# the expression h(x)*f(x)/g(x)
+
 # For nu <- 0.1
 
 # g(x) <- N(1.5, 0.1^2)
+
+IS2 <- function(n, nu)
+{
+    g.x = rnorm(n, 1.5, nu^2)
+    for(i in 1:n)
+    {
+        if(g.x[i] > 2 | g.x[i] < 1)
+        {
+            g.x[i] = 1e6
+        }
+    }
+    g.x = g.x[which(g.x != 1e6)]
+    out = nu*exp(((g.x-1.5)^2)/(2*nu^2) - ((g.x^2)/2))
+    out
+}
+
+nu1 = IS2(100, 0.1)
+nu2 = IS2(100, 1)
+nu3 = IS2(100, 10)
+
+
+hist(nu1)
+hist(nu2)
+hist(nu3)
+
+
+# Our goal should be ~ 0.136, but we're not seeing that yet...
