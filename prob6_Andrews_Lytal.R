ca3097ee9eb2d47841a0ea3a35cfedcb8d474a76
adandrews <adandrews@ucdavis.edu>
2014-05-11 23:07:19 -0700

    Problem 6 Changes

--- prob6_Andrews_Lytal.R
+++ prob6_Andrews_Lytal.R
@@ -38,7 +38,6 @@ g <- function(x, s1, s2)
 
 IMH <- function(n = 1000, t1, t2)
 {
-    browser()
     set.seed(0)
     samps <- numeric(n)             # Define vector to contain samples
     samps[1] <- rgamma(1, t1, t2)   # Define first value of sample
@@ -52,6 +51,7 @@ IMH <- function(n = 1000, t1, t2)
         # u must be less than p.accept to accept a new value
         u = runif(1, 0, 1)
         p.accept = min( (f(y,1.5,2)*g(x,t1,t2))/(f(x,1.5,2)*g(y,t1,t2)), 1) 
+        # p.accept = min( (f(y,t1,t2)*g(x,t1,t2))/(f(x,t1,t2)*g(y,t1,t2)), 1) 
         if(u < p.accept)
         {
             samps[i] = y # Accept the new value
@@ -67,17 +67,55 @@ for(i in 1:100)
 {
     test[i] = mean(IMH(t1 = 1.5, t2 = 2))
 }
+
+
 mean(test)
 ---
 
-test = IMH(t1 = 1, t2 = 1)
+test = IMH(n = 1000000, t1 = 1, t2 = 1)
 
 mean(test)
 mean(1/test)
 
+plot(density(test), main = "Independence - Metropolis-Hastings with Gamma Proposed Density")
 
 target.Ez = sqrt(2/1.5)
 target.E1z = sqrt(1.5/2) + (1/(2*2))
 
 
 # Try with MANY gamma combos: (1,1) and (1,0.5) work best
+
+
+optMeans <- function(theta.1, theta.2){
+	E.z <- sqrt(theta.2/theta.1)
+	E.inv.z <- sqrt(theta.1/theta.2) + 1/(2*theta.2)
+	cbind(E.z = E.z, E.inv.z = E.inv.z)
+}
+
+# Test all possible combinations of theta.1 and theta.2 where each can equal 0.5, 1, 1.5, 2, 5, 7, 10
+
+values <- c(0.5, 1, 1.5, 2, 5, 7, 10)
+
+theta.1 <- rep(values, rep(7, 7))
+theta.2 <- rep(values, 7)
+all.values <- cbind(theta.1, theta.2)
+
+results <- matrix(ncol = 6, nrow = nrow(all.values))
+for(i in 1:nrow(all.values)){
+	t1 <- all.values[i, 1]
+	t2 <- all.values[i, 2]
+	temp <- IMH(n = 1000, t1 = t1, t2 = t2)
+	E.z <- mean(temp)
+	target.E.z <- sqrt(t2/t1)
+	E.inv.z <- mean(1/temp)
+	target.E.inv.z <- sqrt(t1/t2) + 1/(2*t2)
+	abs.diff.E.z <- abs(E.z - target.E.z)
+	abs.diff.E.inv.z <- abs(E.inv.z - target.E.inv.z)
+	results[i, ] <- c(E.z, target.E.z, abs.diff.E.z, E.inv.z, target.E.inv.z, abs.diff.E.inv.z)
+}
+
+colnames(results) <- c("E.z", "target.E.z", "abs.diff.E.z", "E.inv.z", "target.E.inv.z", "abs.diff.E.inv.z")
+
+results <- cbind(all.values, results)
+
+
