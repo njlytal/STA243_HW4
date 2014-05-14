0eb977d7b73809b5ccc13ca6fa78f8dbb497a4f8
adandrews <adandrews@ucdavis.edu>
2014-05-13 23:16:46 -0700

    5 is done

--- prob5_Andrews_Lytal.R
+++ prob5_Andrews_Lytal.R
@@ -51,6 +51,24 @@ func.c <- function(samps = 1e5, n = 100, a = 1, b = 1, p.start = 0.3, lam.start 
 results = func.c(samps = 1e5)
 
 plot(density(results[,1]), main = "Lambda Density")
+abline(v = 2, col = "red")
 plot(density(results[,2]), main = "P Density")
+abline(v = 0.30, col = "red")
 quantile(results[,1], c(0.025,0.975)) # (1.44517, 2.529160)
 quantile(results[,2], c(0.025,0.975)) # (0.2650107, 0.4858813)
+
+values <- c(0.5, 1, 1.5, 2, 5, 10)
+a <- rep(values, rep(6, 6))
+b <- rep(values, 6)
+all.values <- cbind(a, b)
+
+new.results <- sapply(1:nrow(all.values), function(i) func.c(samps = 1e5, n = 100, a = all.values[i, 1], b = all.values[i, 2], p.start = 0.3, lam.start = 2))
+
+confidence.intervals <- lapply(1:ncol(new.results), function(i) cbind(rep(all.values[i, 1],1), rep(all.values[i, 2], 1), cbind(quantile(new.results[, i][[1]], c(0.025,0.975))[1],quantile(new.results[, i][[1]], c(0.025,0.975))[2],quantile(new.results[, i][[2]], c(0.025,0.975))[1], quantile(new.results[, i][[2]], c(0.025,0.975))[2])))
+
+confidence.intervals <- do.call(rbind, confidence.intervals)
+confidence.intervals <- cbind(confidence.intervals, (confidence.intervals[, 3] < 2 & confidence.intervals[, 4] > 2), (confidence.intervals[, 5] < 0.30 & confidence.intervals[, 6] > 0.30))
+rownames(confidence.intervals) = NULL
+colnames(confidence.intervals) = c("a", "b", "lambda 2.5%", "lambda 97.5%", "p 2.5%", "p 97.5%", "2 in ci", "0.3 in ci")
+
+
